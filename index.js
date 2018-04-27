02b17704557db0c0ac1cb5778d88c5e798ceb8eb
firegnu <firegnu@gmail.com>
2018-04-27 15:25:14 +0800

    write log to file

--- index.js
+++ index.js
@@ -3,16 +3,25 @@
  */
 const tracer = require('tracer');
 const config = require('config');
+const fs = require('fs');
 const _ = require('lodash');
 
 var defaultSetting = {
   level: 0,
-  format: '{{timestamp}} <{{title}}> [{{tag}}] {{message}} (in {{file}}:{{line}})'
+  format: '{{timestamp}} <{{title}}> [{{tag}}] {{message}} (in {{file}}:{{line}})',
 };
 
-module.exports = function(tag) {
-  var setting = _.assign({}, defaultSetting, config.tracer);
+module.exports = function(tag, logPath, file) {
   tag = (tag && tag.toUpperCase()) || 'NO-TAG';
+  if(file) {
+    defaultSetting.transport = function(data) {
+      console.log(data.output);
+      fs.appendFile(`${logPath}/${tag}.log`, data.output + '\n', (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+  var setting = _.assign({}, defaultSetting, config.tracer);
   setting.format = setting.format.replace(/\{\{tag}}/g, tag);
   return tracer.colorConsole(setting);
 };
