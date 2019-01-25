4577e6042516ae607d1aa784e4b4f8486defbe93
Colin <biz@colinhan.info>
2019-01-25 18:11:18 +0800

    Make config as optional dependency.

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
  * Created by colinhan on 31/10/2016.
  */
 const tracer = require('tracer');
-const config = require('config');
 const fs = require('fs');
 const _ = require('lodash');
+
+let config;
+try {
+  config = require('config');
+} catch (err) {
+  // Do nothing.
+}
 
 var defaultSetting = {
   level: 0,
@@ -13,7 +19,7 @@ var defaultSetting = {
 
 module.exports = function(tag, logPath) {
   tag = (tag && tag.toUpperCase()) || 'NO-TAG';
-  var setting = _.assign({}, defaultSetting, config.tracer);
+  var setting = config ? _.assign({}, defaultSetting, config.tracer) : defaultSetting;
   if(logPath) {
     setting.transport = function(data) {
       console.log(data.output);
@@ -25,5 +31,3 @@ module.exports = function(tag, logPath) {
   setting.format = setting.format.replace(/\{\{tag}}/g, tag);
   return tracer.colorConsole(setting);
 };
-
-
